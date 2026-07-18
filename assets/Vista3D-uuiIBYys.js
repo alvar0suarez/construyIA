40ec61c07d03fdbc7a669a3e7f317d3f6e763094
alvar0suarez <alvar0suarez@users.noreply.github.com>
2026-07-18 15:43:26 +0000

    deploy: cc2f1885c08f2628c853199746ca2aaf2cf995d3

--- assets/Vista3D-uuiIBYys.js
+++ assets/Vista3D-uuiIBYys.js
@@ -1,4 +1,4 @@
-import{r as Ye,g as mS,j as We,u as As,d as ov,e as gS,P as _S,t as vS}from"./index-Bm5Vmm3A.js";/**
+import{r as Ye,g as mS,j as We,u as As,d as ov,e as gS,P as _S,t as vS}from"./index-DQVEDuUX.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
