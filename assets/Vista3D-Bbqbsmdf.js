513c51cae1cad5050360ff5e2a78abe5db95c8a3
alvar0suarez <alvar0suarez@users.noreply.github.com>
2026-07-28 10:54:16 +0000

    deploy: 4ec51dc8c0bc5ace5812676fca6414d8d83cb76e

--- assets/Vista3D-Bbqbsmdf.js
+++ assets/Vista3D-Bbqbsmdf.js
@@ -1,4 +1,4 @@
-import{r as We,g as vS,j as Oe,u as Yr,d as ux,P as vu,t as Oo,e as yS,C as SS}from"./index-Bb21L5uY.js";/**
+import{r as We,g as vS,j as Oe,u as Yr,d as ux,P as vu,t as Oo,e as yS,C as SS}from"./index-DY7U0s4x.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
